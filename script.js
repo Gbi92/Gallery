f8a4c22c2eebd248f995422d57df4f0cb6093f38
Gbi92 <70910864+Gbi92@users.noreply.github.com>
2020-09-18 16:54:57 +0200

    added jquery to photo viewer

---
+++ script.js
@@ -0,0 +1,72 @@
+
+let currentPhoto = 0;
+let imagesData = [
+    {
+        photo: './images/image1.jpg',
+        title: 'Lago di Braies',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+    },
+    {
+        photo: './images/image2.jpg',
+        title: 'Sunset in the Mountains',
+        description: 'Diam sollicitudin tempor id eu nisl. Congue quisque egestas diam in.'
+    },
+    {
+        photo: './images/image3.jpg',
+        title: 'Sunrise',
+        description: 'Nibh venenatis cras sed felis eget. Sit amet purus gravida quis.'    
+    },
+    {
+        photo: './images/image4.jpg',
+        title: 'Sunset',
+        description: 'Dignissim enim sit amet venenatis urna cursus eget nunc. Nibh ipsum consequat nisl vel.'
+    },
+    {
+        photo: './images/image5.jpg',
+        title: 'Where the Wild Things Are',
+        description: 'Nunc id cursus metus aliquam eleifend mi. Pellentesque habitant morbi tristique senectus.'
+    },
+    {
+        photo: './images/image6.jpg',
+        title: 'Poppy field',
+        description: 'Porta non pulvinar neque laoreet suspendisse. Amet tellus cras adipiscing enim.'
+    },
+    {
+        photo: './images/image7.jpg',
+        title: 'Hay field',
+        description: 'Placerat duis ultricies lacus sed turpis tincidunt id. Commodo viverra maecenas accumsan lacus vel facilisis volutpat.'
+    },
+    {
+        photo: './images/image8.jpg',
+        title: 'A Walk in the Snow',
+        description: 'Fringilla phasellus faucibus scelerisque eleifend donec pretium. Non nisi est sit amet facilisis.'
+    }
+];
+
+let loadPhoto = (photoNumber) => {
+    $('#photo').attr('src', imagesData[photoNumber].photo);
+    $('#photo-title').text(imagesData[photoNumber].title);
+    $('#photo-description').text(imagesData[photoNumber].description);
+}
+
+loadPhoto(currentPhoto);
+
+$('#rightArrow').click(() => {
+    if(currentPhoto < imagesData.length-1) {
+        currentPhoto++;
+        loadPhoto(currentPhoto);
+    }else{
+        currentPhoto = 0
+        loadPhoto(currentPhoto);
+    }
+});
+
+$('#leftArrow').click(() => {  
+    if(currentPhoto == 0 ){
+        currentPhoto = imagesData.length-1;
+        loadPhoto(currentPhoto);
+    }else{
+        currentPhoto--;
+        loadPhoto(currentPhoto);
+    }
+});
